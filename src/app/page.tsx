63c498a5806c393ffd54e67a66927de4f6b32773
ale <ale@snoozed.dev>
2023-07-07 18:01:55 -0300

    github deploy test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-24 px-4">
-      <h1 className="text-3xl sm:text-7xl">not ready yet</h1>
+      <h1 className="text-3xl sm:text-7xl">not ready yet.</h1>
     </main>
   );
 }
