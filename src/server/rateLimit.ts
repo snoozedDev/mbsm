0d8cee6c91d251214950eeff25aa2a3789487253
ale <ale@snoozed.dev>
2023-07-10 20:20:40 -0300

    more sensible rate limit

--- src/server/rateLimit.ts
+++ src/server/rateLimit.ts
@@ -4,7 +4,7 @@ import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 // Create a new ratelimiter, that allows 10 requests per 10 seconds
 export const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(10, "10 m"),
+  limiter: Ratelimit.slidingWindow(10, "10 s"),
   analytics: true,
   /**
    * Optional prefix for the keys used in redis. This is useful if you want to share a redis
