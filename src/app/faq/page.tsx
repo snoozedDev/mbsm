a2fa0b45f7f19ae830c78ce20f7343f5113bfdce
ale <ale@snoozed.dev>
2023-07-10 21:07:19 -0300

    set theme

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,5 +1,26 @@
+import { ThemeManager } from "@/components/theme-manager";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 const FAQ = () => {
-  return <div>FAQ</div>;
+  return (
+    <main>
+      FAQ
+      <ThemeManager />
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </main>
+  );
 };
 
 export default FAQ;
